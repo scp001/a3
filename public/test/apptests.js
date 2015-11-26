c686eb6afd515c1d723d54979fbaec07dad646b5
Zhizhou (Leo) Song <songzhi@cms-temp.utsc.utoronto.ca>
2015-11-25 23:51:59 -0500

    add 5 test cases

--- public/test/apptests.js
+++ public/test/apptests.js
@@ -18,10 +18,10 @@ QUnit.jUnitReport = function(report) {
 
  test( "Inspect jQuery.getJSON's usage of jQuery.ajax", function() {
     this.spy( jQuery, "ajax" );
-    var getJSONDone = jQuery.getJSON( "http://cms-chorus.utsc.utoronto.ca:28012/movies" );
+    var getJSONDone = jQuery.getJSON( "https://mathlab.utsc.utoronto.ca:41485/movies/" );
     ok( jQuery.ajax.calledOnce );
     equal( jQuery.ajax.getCall(0).args[0].url,
-	"http://cms-chorus.utsc.utoronto.ca:28012/movies" );
+	"https://mathlab.utsc.utoronto.ca:41485/movies/" );
     equal( jQuery.ajax.getCall(0).args[0].dataType, "json" );
   });
 
@@ -145,10 +145,166 @@ QUnit.jUnitReport = function(report) {
   });
 
 // added test case
-test("Test movie-deletion succeeds if session is authenticated.", function(assert){});
-test("Test review-add triggers an error event if unauthenticated.", function(assert){});
-test("Test review model/collection add/save, and callback functions.", function(assert){
+  test("Test movie-deletion fails by different user.", function(assert){
+	assert.expect( 3 );
+    var done1 = assert.async();
+    var done2 = assert.async();
+    var done3 = assert.async();
+    var movie = new splat.Movie();  // model
+    movie.set("_id", "5650bf6b6f3c0a143c50994e");
+    movie.urlRoot = '/movies';
+	
+    // fetch existing movie model
+    var movieFetch = movie.fetch({
+        success: function(movie, resp) {
+            assert.equal( resp._id, "5650bf6b6f3c0a143c50994e",
+				"Successful movie fetch" );
+			done1();
+        }
+    });
+	
+    // authenticate user with valid credentials
+    var user = new splat.User({username:"b", password:"b", login: 1});
+    var auth = user.save(null, {
+        type: 'put',
+        success: function (model, resp) {
+            assert.equal( resp.username, "b",
+				"Successful login with valid credentials" );
+            done2();
+        }
+    });
+	
+    $.when(movieFetch, auth).done(function() {
+        // attempt to delete movie
+        movie.destroy({
+    	    error: function (model, resp) {
+    	        assert.equal( resp.status, 403, 
+						"Movie was not successfully deleted by a different user");
+				done3();
+    	    }
+        });
+    });
+  });
 
-});
-test("Image Resize?", function(assert){});
-test("User add?", function(assert){});
+  test("Test movie-deletion succeeds if session is authenticated.", function(assert){
+	assert.expect( 3 );
+    var done1 = assert.async();
+    var done2 = assert.async();
+    var done3 = assert.async();
+    var movie = new splat.Movie();  // model
+    movie.set("_id", "5650bf6b6f3c0a143c50994e");
+    movie.urlRoot = '/movies';
+	
+    // fetch existing movie model
+    var movieFetch = movie.fetch({
+        success: function(movie, resp) {
+            assert.equal( resp._id, "5650bf6b6f3c0a143c50994e",
+				"Successful movie fetch" );
+			done1();
+        }
+    });
+	
+    // authenticate user with valid credentials
+    var user = new splat.User({username:"a", password:"a", login: 1});
+    var auth = user.save(null, {
+        type: 'put',
+        success: function (model, resp) {
+            assert.equal( resp.username, "a",
+				"Successful login with valid credentials" );
+            done2();
+        }
+    });
+	
+    $.when(movieFetch, auth).done(function() {
+        // attempt to delete movie
+        movie.destroy({
+    	    success: function (model, resp) {
+    	        assert.equal( true, "Movie was successfully deleted");
+				done3();
+    	    }
+        });
+    });
+  });
+
+  test("Fires a custom event when the state changes to review.", function() {
+    var changeModelCallback = this.spy();
+    var review = new splat.Review();
+    review.bind( "change", changeModelCallback );
+    review.set( { "reviewname": "Leo" } );
+    ok( changeModelCallback.calledOnce,
+	"A change event-callback was correctly triggered" );
+  });
+
+   test("Test review model/collection add/save, and callback functions.", function(assert) {
+    assert.expect(4);   // 4 assertions to be run
+    var review = new splat.Review({"__v":0, freshness: 1.0, reviewtext: "leo",
+        reviewname: "leo2", reviewaffil: "leo3",});  // model
+    var reviews = new splat.Reviews();  // collection
+    // verify Reviews-collection URL
+    equal( reviews.url, "/movies/:id/reviews",
+		"correct URL set for instantiated Reviews collection" );
+    // test "add" event callback when review added to collection
+    var addModelCallback = this.spy();
+    reviews.bind( "add", addModelCallback );
+    reviews.add(review);
+    ok( addModelCallback.called,
+		 "add callback triggered by reviews collection add()" );
+	
+	// make sure user is logged out
+    var user = new splat.User({username:"a", password:"a"});
+    var auth = user.save(null, {
+        type: 'put',
+	success: function (model, resp) {
+	    assert.deepEqual( resp, {}, "Signout returns empty response object" );
+    	    done1();
+
+	}
+    });
+    auth.done(function() { 
+	movie.save(null, {
+	    error: function (model, error) {
+	        assert.equal( error.status, 403,
+		    "Saving without authentication returns 403 status");
+	        done2();
+	    }
+       });
+    });
+  });
+  
+  test("Test review-save succeeds if session is authenticated.", function(assert) {
+    assert.expect( 3 );
+    var done1 = assert.async();
+    var done2 = assert.async();
+    var done3 = assert.async();
+    var review = new splat.Review();  // model
+    review.set("_id", "5650bf6b6f3c0a143c50994e");
+    review.urlRoot = '/movies/:id/reviews';
+    // fetch existing review model
+    var reviewFetch = review.fetch({
+        success: function(review, resp) {
+            assert.equal( resp._id, "5650bf6b6f3c0a143c50994e",
+		"Successful review fetch" );
+	    done1();
+        }
+    });
+    // authenticate user with valid credentials
+    var user = new splat.User({username:"a", password:"a", login: 1});
+    var auth = user.save(null, {
+        type: 'put',
+        success: function (model, resp) {
+            assert.equal( resp.username, "a",
+		"Successful login with valid credentials" );
+            done2();
+        }
+    });
+    $.when(reviewFetch, auth).done(function() {
+        // attempt to update existing review
+        review.save({"reviewname": "QUnit!"}, {
+    	    success: function (model, resp) {
+    	        assert.equal( resp.title, "QUnit!",
+					"Saving model update succeeds when logged in" );
+					done3();
+			}
+		});
+    });
+  });
